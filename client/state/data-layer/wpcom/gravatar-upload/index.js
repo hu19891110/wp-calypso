502b49ff07454fc9dbc093e866629fbdcd67b4f4
Payton Swick <payton@foolord.com>
2017-07-04 14:44:47 -0400

    Always return Promise in announceSuccess for consistency

--- client/state/data-layer/wpcom/gravatar-upload/index.js
+++ client/state/data-layer/wpcom/gravatar-upload/index.js
@@ -31,13 +31,16 @@ export function uploadGravatar( { dispatch }, action ) {
 }
 
 export function announceSuccess( { dispatch }, { file }, next, data ) {
-	// Sometimes wpcom-proxy-request reports a failed connection as a success, so
-	// we test for the response here. See
-	// https://github.com/Automattic/wp-calypso/pull/15636#issuecomment-312860944
-	if ( ! data.success ) {
-		return announceFailure( { dispatch } );
-	}
-	return new Promise( ( resolve ) => {
+	// Return a Promise for testing purposes
+	return new Promise( ( resolve, reject ) => {
+		// Sometimes wpcom-proxy-request reports a failed connection as a success, so
+		// we test for the response here. See
+		// https://github.com/Automattic/wp-calypso/pull/15636#issuecomment-312860944
+		if ( ! data.success ) {
+			announceFailure( { dispatch } );
+			return reject();
+		}
+
 		const fileReader = new FileReader( file );
 		fileReader.addEventListener( 'load', () => {
 			resolve( fileReader.result );
