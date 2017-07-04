daf11b9b6d978ffec85f5874a29dcc0554c28f85
Kirk Wight <kwight@kwight.ca>
2017-07-04 13:27:40 -0700

    Improve error handling and redirects for /comments routing.

--- client/my-sites/comments/controller.js
+++ client/my-sites/comments/controller.js
@@ -12,15 +12,32 @@ import route from 'lib/route';
 import CommentsManagement from './main';
 import controller from 'my-sites/controller';
 
+export const isStatusValid = status => {
+	const validStatuses = [ 'pending', 'approved', 'spam', 'trash', 'all' ];
+	return validStatuses.indexOf( status ) !== -1;
+};
+
 export const comments = function( context ) {
+	const status = context.params.status;
 	const siteSlug = route.getSiteFragment( context.path );
-	const status = context.params.status === 'pending' ? 'unapproved' : context.params.status;
+
+	if ( ! isStatusValid( status ) && ! siteSlug ) {
+		page.redirect( '/comments' );
+	}
+
+	if ( ! isStatusValid( status ) && siteSlug ) {
+		page.redirect( `/comments/pending/${ siteSlug }` );
+	}
+
+	if ( ! siteSlug ) {
+		page.redirect( `/comments/${ status }` );
+	}
 
 	renderWithReduxStore(
 		<CommentsManagement
 			basePath={ context.path }
 			siteSlug={ siteSlug }
-			status={ status }
+			status={ 'pending' === status ? 'unapproved' : status }
 		/>,
 		'primary',
 		context.store
@@ -34,5 +51,10 @@ export const sites = function( context ) {
 	if ( status === siteSlug ) {
 		return page.redirect( `/comments/pending/${ siteSlug }` );
 	}
+
+	if ( ! isStatusValid( status ) ) {
+		return page.redirect( '/comments' );
+	}
+
 	controller.sites( context );
 };
